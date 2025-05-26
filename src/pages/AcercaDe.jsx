bb41b129c5d733a8f274c81f554900833f59c658
vlentinLZN . <diegovalentinlozano@gmail.com>
2025-05-26 20:52:20 -0300

    AcercaDe y Home agregados Valentin Lozano

---
+++ src/pages/AcercaDe.jsx
@@ -0,0 +1,15 @@
+function AcercaDe() {
+  return (
+    <div>
+      <h2>Acerca de</h2>
+      <p>Gestión de alumnos - Grupo 16 - Programación Visual 2025 - APU - Versión 1.0 .</p>
+      <p>Integrantes: </p>
+      <p> <b> Gallo   Tiziano </b></p>
+      <p> <b> Lozano  Valentin</b></p>
+      <p> <b> Mamani  Mariano </b></p>
+      <p> <b> Miranda Cesar   </b></p>
+    </div>
+  );
+}
+
+export default AcercaDe;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="text-center">
+      <h1>Gestión de Alumnos</h1>
+      <p>Administración de información de estudiantes.</p>
+    </div>
+  );
+}
+
+export default Home;
