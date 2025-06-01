9986266067b236efa16d5b82baee2fc3e5e2c8b5
Cesar Miranda <cesargermanmiranda@gmail.com>
2025-06-01 18:20:20 -0300

    Depuracion NavBar y otros. Agregado por Cesar Miranda

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,26 @@
 
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function NavBar(){
 
   return(
     <>
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar  expand="lg" className="bg-body-tertiary">
             <Container>
               <Navbar.Brand> Gestión Alumnos </Navbar.Brand>
               <Navbar.Toggle aria-controls="bsic-navbar-nav"/>
               <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="me-auto">
                     <NavDropdown title="Seleccione Tarea" id="basic-nav-dropdown">
-                      <NavDropdown.Item href="/"            > Inicio           </NavDropdown.Item>
-                      <NavDropdown.Item href="/ListaAlumnos"> Lista de Alumnos </NavDropdown.Item>
-                      <NavDropdown.Item href="/NuevoAlumno" > Nuevo Alumno     </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/"       >       Inicio           </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/alumnos">       Lista de Alumnos </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/alumnos/nuevo"> Nuevo Alumno     </NavDropdown.Item>
                       <NavDropdown.Divider/>
-                      <NavDropdown.Item href="/AcercaDe"    > Acerca de        </NavDropdown.Item>    
+                      <NavDropdown.Item as={Link} to="/acerca">        Acerca de        </NavDropdown.Item>    
                     </NavDropdown>
-                    
+
                   </Nav>
               </Navbar.Collapse>
             </Container>
