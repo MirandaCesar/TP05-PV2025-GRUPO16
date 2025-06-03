7184176416ec6ba6e020b05dcaa8870feea60ae2
Cesar Miranda <cesargermanmiranda@gmail.com>
2025-06-02 21:19:15 -0300

    Modificacion malas referencias en app. Subido por Cesar Miranda

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
-import ListaAlumnos from './pages/ListaAlumnos';
-import NuevoAlumno  from './pages/NuevoAlumno';
 import AcercaDe     from './pages/AcercaDe';
+import { useState } from 'react';
 
-function App() {
+import AlumnosList from './components/AlumnosList'
+import AlumnoForm from './components/AlumnoForm';
+import AlumnoDetails from './components/AlumnoDetails';
+
+const App = () => {
+  const [alumnos, setAlumnos] = useState([]);
+
   return (
     <Router>
       <NavBar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/"             element={<Home         />} />
-          <Route path="/ListaAlumnos" element={<ListaAlumnos />} />
-          <Route path="/NuevoAlumno"  element={<NuevoAlumno  />} />
-          <Route path="/AcercaDe"     element={<AcercaDe     />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/"                   element={<Home                                                    />} />
+        <Route path="/alumnos"            element={<AlumnosList   alumnos={alumnos} setAlumnos={setAlumnos} />} />
+        <Route path="/alumnos/nuevo"      element={<AlumnoForm    alumnos={alumnos} setAlumnos={setAlumnos} />} />
+        <Route path="/alumnos/:id/editar" element={<AlumnoForm    alumnos={alumnos} setAlumnos={setAlumnos} />} />
+        <Route path="/alumnos/:id"        element={<AlumnoDetails alumnos={alumnos}                         />} />
+        <Route path="/acerca"             element={<AcercaDe/>}                                                 />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
